02e1fcd13295f9d54322c6b58f261e911e0a9995
Bushra Aksoy <bbxsra17@gmail.com>
2024-05-03 02:31:37 +0500

    removed hooks

--- src/Hooks/useAuthToken.jsx
+++
@@ -1,9 +0,0 @@
-const useAuthToken = () => {
-  const setAuthToken = (token) => localStorage.setItem("token", token);
-  const getAuthToken = () => localStorage.getItem("token");
-  const clearAuthToken = () => localStorage.removeItem("token");
-
-  return { setAuthToken, getAuthToken, clearAuthToken };
-};
-
-export default useAuthToken;
